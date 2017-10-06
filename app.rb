05e4f96bc309df60a201fa700b83fe74949a738d
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-06 11:35:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships] each do |elements|
+        Ship.new(elements)
+      end
+      @ships = Ship.all
+
+      erb :show
+
+      
+    end
   end
 end
