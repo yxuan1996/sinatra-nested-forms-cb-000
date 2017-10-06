fc5bcfcf1b82650f32c43229dcb5585ee5bd25c1
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-06 11:51:02 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,17 +1,17 @@
 class Pirate
   attr_accessor :name, :weight, :height
 
-  @@pirates = []
+  PIRATES = []
 
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    pirates.push(self)
+    PIRATES.push(self)
   end
 
   def self.all
-    @@pirates
+    PIRATES
   end
 
 end
