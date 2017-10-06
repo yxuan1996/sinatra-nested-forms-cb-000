f359f016126bc02909d6ce4d9a074ccd60c5ddc3
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-06 11:53:00 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships=[]
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@ships.push(self)
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
 end
